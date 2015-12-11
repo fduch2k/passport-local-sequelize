cda44956a4bde1d4d36dfdd0f8e5eddfccd1c56b
Alexander Hramov <alexander.hramov@gmail.com>
2015-12-11 11:49:20 +0300

    Update to sequelize 3 and add test for resetPassword

--- test/all_spec.js
+++ test/all_spec.js
@@ -1,3 +1,4 @@
+/*jshint expr: true*/
 /* global describe, before, beforeEach, it */
 
 var Sequelize = require('sequelize'),
@@ -13,22 +14,24 @@ var db = new Sequelize('test-db', 'user', 'pass', {
 var User;
 
 var initDb = function (done) {
-    User = passportLocalSequelize.defineUser(db, null, {
-        iterations: 1000
+    User = passportLocalSequelize.defineUser(db, {
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        }
+    }, {
+        usernameQueryFields: ['email'],
+        iterations: 50
     });
 
     // Authenticate the db
-    db.authenticate()
-        .complete(function (err) {
-            if (err) {
-                return done(err);
-            }
-
-            // Synchronize the db
-            db.sync({ force: true }).complete(function (err) {
-                done(err);
-            });
-        });
+    return db.authenticate().then(function () {
+        // Synchronize the db
+        return db.sync({ force: true });
+    }).then(function () {
+        done();
+    }).catch(done);
 };
 
 describe('Passport Local Sequelize', function () {
@@ -49,11 +52,24 @@ describe('Passport Local Sequelize', function () {
         should.exist(User);
     });
 
+    it('can find user by email', function (done) {
+        should.exist(User.findByUsername);
+        User.register({username: 'someuser', email: 'test@example.com'}, 'somepass').then(function () {
+            return User.findByUsername('test@example.com');
+        }).then(function (user) {
+            should(user).be.instanceof(User.Instance).and.have.property('email', 'test@example.com');
+            done();
+        }).catch(function (error) {
+            done(error);
+        });
+    });
+
     it('can register and authenticate a user', function (done) {
         should.exist(User.register);
 
-        User.register('someuser', 'somepass').then(function (registeredUser) {
+        User.register({username: 'someuser', email: 'test@example.com'}, 'somepass').then(function (registeredUser) {
             registeredUser.get('username').should.equal('someuser');
+            registeredUser.get('email').should.equal('test@example.com');
             registeredUser.get('id').should.be.above(0);
 
             registeredUser.authenticate('badpass', function (err, authenticated) {
@@ -67,6 +83,35 @@ describe('Passport Local Sequelize', function () {
                         return done(err);
                     }
 
+                    authenticatedUser.should.not.equal(false);
+                    authenticatedUser.get('username').should.equal('someuser');
+                    done();
+                });
+            });
+        }).catch(function (error) {
+            done(error);
+        });
+    });
+
+    it('can reset password', function (done) {
+        should.exist(User.resetPassword);
+        User.register({username: 'someuser', email: 'test@example.com'}, 'somepass').then(function () {
+            return User.setResetPasswordKey('someuser');
+        }).then(function (user) {
+            user.get('username').should.equal('someuser');
+            user.get('resetPasswordKey').should.be.ok;
+            return User.resetPassword('someuser', 'passsome', user.get('resetPasswordKey'));
+        }).then(function (user) {
+            user.authenticate('somepass', function (err, authenticated) {
+                if (err) {
+                    return done(err);
+                }
+                authenticated.should.equal(false);
+
+                user.authenticate('passsome', function (err, authenticatedUser) {
+                    if (err) {
+                        return done(err);
+                    }
                     authenticatedUser.should.not.equal(false);
                     authenticatedUser.get('username').should.equal('someuser');
                     done();
